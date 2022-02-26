fbcf77686ffb9e70b6d120b7d61565c75402c591
padilla <padilla@speedycontrol.com>
2022-02-26 01:30:29 +0100

    Se ajusta bazel para que funcione java17

--- tls/socket/custom/src/main/java/jtpadilla/tls/socket/custom/SSLCustomClient.java
+++ tls/socket/custom/src/main/java/jtpadilla/tls/socket/custom/SSLCustomClient.java
@@ -1,7 +1,9 @@
 package jtpadilla.tls.socket.custom;
 
 import javax.net.ssl.*;
-import java.security.KeyStore;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 public class SSLCustomClient {
 
@@ -9,26 +11,50 @@ public class SSLCustomClient {
 
    public SSLCustomClient(String address, int port) throws Exception {
 
+      KeyManagerFactory kmf = getKeyManagerFactory();
+      TrustManagerFactory tmf = getTrustManagerFactory();
+
+      SSLContext sc = getSSLContext(kmf, tmf);
+
+      SSLSocketFactory ssf = sc.getSocketFactory();
+
+      client = (SSLSocket) ssf.createSocket(address, port);
+      client.startHandshake();
+
+   }
+
+   private SSLContext getSSLContext(KeyManagerFactory kmf, TrustManagerFactory tmf) throws NoSuchAlgorithmException, KeyManagementException {
+
+      SSLContext sc = SSLContext.getInstance("TLSv1.2");
+
+      TrustManager[] trustManagers = tmf.getTrustManagers();
+      KeyManager[] keyManagers = kmf.getKeyManagers();
+
+      sc.init(keyManagers, trustManagers, SecureRandom.getInstance("SHA1PRNG"));
+      return sc;
+
+   }
+
+   private KeyManagerFactory getKeyManagerFactory() throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException, UnrecoverableKeyException {
+
       KeyStore keyStore = KeyStore.getInstance("JKS");
       keyStore.load(Util.getClientKeyJks(), "clientpass".toCharArray());
 
       KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
       kmf.init(keyStore, "clientpass".toCharArray());
+      return kmf;
+
+   }
+
+   private TrustManagerFactory getTrustManagerFactory() throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException {
 
       KeyStore trustedStore = KeyStore.getInstance("JKS");
       trustedStore.load(Util.getCientTrustedCertsJks(), "clientpass".toCharArray());
 
       TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
       tmf.init(trustedStore);
+      return tmf;
 
-      SSLContext sc = SSLContext.getInstance("TLSv1.2");
-      TrustManager[] trustManagers = tmf.getTrustManagers();
-      KeyManager[] keyManagers = kmf.getKeyManagers();
-      sc.init(keyManagers, trustManagers, null);
-
-      SSLSocketFactory ssf = sc.getSocketFactory();
-      client = (SSLSocket) ssf.createSocket(address, port);
-      client.startHandshake();
    }
 
    public void start() {
--- tls/socket/custom/src/main/java/jtpadilla/tls/socket/custom/SSLCustomServer.java
+++ tls/socket/custom/src/main/java/jtpadilla/tls/socket/custom/SSLCustomServer.java
@@ -1,33 +1,59 @@
 package jtpadilla.tls.socket.custom;
 
 import javax.net.ssl.*;
-import java.security.KeyStore;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 public class SSLCustomServer {
+
    private SSLServerSocket serverSocket;
 
    public SSLCustomServer(int port) throws Exception {
+
+      KeyManagerFactory kmf = getKeyManagerFactory();
+      TrustManagerFactory tmf = getTrustManagerFactory();
+
+      SSLContext sc = getSSLContext(kmf, tmf);
+
+      SSLServerSocketFactory ssf = sc.getServerSocketFactory();
+
+      serverSocket = (SSLServerSocket) ssf.createServerSocket(port);
+      
+   }
+
+   private SSLContext getSSLContext(KeyManagerFactory kmf, TrustManagerFactory tmf) throws NoSuchAlgorithmException, KeyManagementException {
+
+      SSLContext sc = SSLContext.getInstance("TLSv1.2");
+
+      TrustManager[] trustManagers = tmf.getTrustManagers();
+      KeyManager[] keyManagers = kmf.getKeyManagers();
+
+      sc.init(keyManagers, trustManagers, SecureRandom.getInstance("SHA1PRNG"));
+      return sc;
+
+   }
+
+   private KeyManagerFactory getKeyManagerFactory() throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException, UnrecoverableKeyException {
 
       KeyStore keyStore = KeyStore.getInstance("JKS");
       keyStore.load(Util.getServerKeyJks(), "servpass".toCharArray());
 
       KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
       kmf.init(keyStore, "servpass".toCharArray());
+      return kmf;
+
+   }
+
+   private TrustManagerFactory getTrustManagerFactory() throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException {
 
       KeyStore trustedStore = KeyStore.getInstance("JKS");
       trustedStore.load(Util.getServerTrustedCertsJks(), "servpass".toCharArray());
 
       TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
       tmf.init(trustedStore);
+      return tmf;
 
-      SSLContext sc = SSLContext.getInstance("TLSv1.2");
-      TrustManager[] trustManagers = tmf.getTrustManagers();
-      KeyManager[] keyManagers = kmf.getKeyManagers();
-      sc.init(keyManagers, trustManagers, null);
-
-      SSLServerSocketFactory ssf = sc.getServerSocketFactory();
-      serverSocket = (SSLServerSocket) ssf.createServerSocket(port);
-      
    }
 
    public void start() {
