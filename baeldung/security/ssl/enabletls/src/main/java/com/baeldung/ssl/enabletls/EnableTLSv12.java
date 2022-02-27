727c75ae1be590ed8f18d1354adabbc5df969a84
padilla <padilla@speedycontrol.com>
2022-02-27 20:34:50 +0100

    Nuevos ejemplos

--- baeldung/security/ssl/enabletls/src/main/java/com/baeldung/ssl/enabletls/EnableTLSv12.java
+++ baeldung/security/ssl/enabletls/src/main/java/com/baeldung/ssl/enabletls/EnableTLSv12.java
@@ -36,10 +36,10 @@ public class EnableTLSv12 {
                 Integer.parseInt(Objects.requireNonNull(args[1]))
         );
 
-//        enableTLSv12.enableTLSv12UsingHttpConnection();
+//        enableTLSv12.enableTLSv12UsingSSLParameters();
 //        enableTLSv12.enableTLSv12UsingProtocol();
         enableTLSv12.enableTLSv12UsingSSLContext();
-//        enableTLSv12.enableTLSv12UsingSSLParameters();
+//        enableTLSv12.enableTLSv12UsingHttpConnection();
 
     }
 
@@ -75,27 +75,52 @@ public class EnableTLSv12 {
     }
 
     public void enableTLSv12UsingSSLParameters() throws UnknownHostException, IOException {
+
         SSLSocketFactory socketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
         SSLSocket sslSocket = (SSLSocket) socketFactory.createSocket(url.trim(), port);
+
+        // Asignandole SSLParameters al SSLSocket
         SSLParameters params = new SSLParameters();
         params.setProtocols(new String[] { "TLSv1.2" });
         sslSocket.setSSLParameters(params);
+
         sslSocket.startHandshake();
         handleCommunication(sslSocket, "SSLSocketFactory-SSLParameters");
+
     }
 
     public void enableTLSv12UsingProtocol() throws IOException {
+
         SSLSocketFactory socketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
         SSLSocket sslSocket = (SSLSocket) socketFactory.createSocket(url, port);
+
+        // Modificando el SSLSocket
         sslSocket.setEnabledProtocols(new String[] { "TLSv1.2" });
+
         sslSocket.startHandshake();
         handleCommunication(sslSocket, "SSLSocketFactory-EnabledProtocols");
+
+    }
+
+    public void enableTLSv12UsingSSLContext() throws NoSuchAlgorithmException, KeyManagementException, UnknownHostException, IOException {
+
+        SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+        sslContext.init(null, null, new SecureRandom());
+
+        SSLSocketFactory socketFactory = sslContext.getSocketFactory();
+
+        SSLSocket socket = (SSLSocket) socketFactory.createSocket(url, port);
+
+        handleCommunication(socket, "SSLContext");
+
     }
 
     public void enableTLSv12UsingHttpConnection() throws IOException, NoSuchAlgorithmException, KeyManagementException {
-        URL urls = new URL("https://" + url + ":" + port);
+
         SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
         sslContext.init(null, null, new SecureRandom());
+
+        URL urls = new URL("https://" + url + ":" + port);
         HttpsURLConnection connection = (HttpsURLConnection) urls.openConnection();
         connection.setSSLSocketFactory(sslContext.getSocketFactory());
         try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
@@ -105,14 +130,6 @@ public class EnableTLSv12 {
             }
         }
         logger.info("Created TLSv1.2 connection on HttpsURLConnection");
-    }
-
-    public void enableTLSv12UsingSSLContext() throws NoSuchAlgorithmException, KeyManagementException, UnknownHostException, IOException {
-        SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
-        sslContext.init(null, null, new SecureRandom());
-        SSLSocketFactory socketFactory = sslContext.getSocketFactory();
-        SSLSocket socket = (SSLSocket) socketFactory.createSocket(url, port);
-        handleCommunication(socket, "SSLContext");
     }
 
 }
