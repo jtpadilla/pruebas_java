dd5f28f7c107e6d4b30d40146f56a6bd05277395
padilla <padilla@speedycontrol.com>
2022-03-03 00:23:34 +0100

    Nuevos ejemplos

--- tls/tls3/gypsyengineer/modular/lib/client/src/main/java/com/gypsyengineer/tlsbunny/jsse/lib/client/ClientSession.java
+++ tls/tls3/gypsyengineer/modular/lib/client/src/main/java/com/gypsyengineer/tlsbunny/jsse/lib/client/ClientSession.java
@@ -4,6 +4,7 @@ import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 public class ClientSession implements AutoCloseable {
 
