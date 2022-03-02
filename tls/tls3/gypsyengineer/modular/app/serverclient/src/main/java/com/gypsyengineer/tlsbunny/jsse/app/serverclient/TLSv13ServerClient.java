1b18905513b1b099083157148509f816f687d327
Juan Padilla <padilla@speedycontrol.com>
2022-03-02 20:32:15 +0100

    Pruebas TLS

--- tls/tls3/gypsyengineer/modular/app/serverclient/src/main/java/com/gypsyengineer/tlsbunny/jsse/app/serverclient/TLSv13ServerClient.java
+++ tls/tls3/gypsyengineer/modular/app/serverclient/src/main/java/com/gypsyengineer/tlsbunny/jsse/app/serverclient/TLSv13ServerClient.java
@@ -1,8 +1,13 @@
 package com.gypsyengineer.tlsbunny.jsse.app.serverclient;
 
+import com.gypsyengineer.tlsbunny.jsse.lib.client.ClientLauncher;
+import com.gypsyengineer.tlsbunny.jsse.lib.server.ServerListener;
+
 public class TLSv13ServerClient {
 
     public static void main(String[] args) throws Exception {
+        ServerListener.runListenerForever();
+        ClientLauncher.runClientOneTime();
     }
 
 }
