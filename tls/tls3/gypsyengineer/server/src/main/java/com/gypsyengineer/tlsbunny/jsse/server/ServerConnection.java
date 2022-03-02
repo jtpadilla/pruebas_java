02f712ef786269370a83a23e20be32b379f6b336
Juan Padilla <padilla@speedycontrol.com>
2022-03-02 18:55:06 +0100

    Se activa la compilacion con java 17

--- tls/tls3/gypsyengineer/server/src/main/java/com/gypsyengineer/tlsbunny/jsse/server/ServerConnection.java
+++ tls/tls3/gypsyengineer/server/src/main/java/com/gypsyengineer/tlsbunny/jsse/server/ServerConnection.java
@@ -2,6 +2,8 @@ package com.gypsyengineer.tlsbunny.jsse.server;
 
 import javax.net.ssl.SSLSocket;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,13 +13,31 @@ public class ServerConnection implements Runnable {
     static private AtomicInteger sessionCounter = new AtomicInteger(0);
 
     final private SSLSocket socket;
-    final private int sessionId;
     final private Logger logger;
+    final private InputStream is;
+    final private OutputStream os;
 
-    public ServerConnection(SSLSocket socket) {
+    public ServerConnection(SSLSocket socket) throws IOException {
+        logger = Logger.getLogger(String.format("server.%d", sessionCounter.getAndIncrement()));
         this.socket = socket;
-        this.sessionId = sessionCounter.getAndIncrement();
-        logger = Logger.getLogger(String.format("server.%d", this.sessionId));
+        this.is = new BufferedInputStream(socket.getInputStream());
+        this.os = new BufferedOutputStream(socket.getOutputStream());
+    }
+
+    private Optional<String> readLine() throws IOException {
+        byte[] data = new byte[2048];
+        int len = is.read(data);
+        if (len <= 0) {
+            return Optional.empty();
+        } else {
+            return Optional.of(new String(data, 0, len));
+        }
+    }
+
+    private void writeLine(String line) throws IOException {
+        byte[] bytes = line.getBytes(StandardCharsets.UTF_8);
+        os.write(bytes, 0, bytes.length);
+        os.flush();
     }
 
     @Override
@@ -26,18 +46,15 @@ public class ServerConnection implements Runnable {
         logger.info("accepted");
 
         try {
-            InputStream is = new BufferedInputStream(socket.getInputStream());
-            OutputStream os = new BufferedOutputStream(socket.getOutputStream());
-
-            byte[] data = new byte[2048];
-            int len = is.read(data);
-            if (len <= 0) {
-                throw new IOException("no data received");
+            while(true) {
+                Optional<String> line = readLine();
+                if (line.isPresent()) {
+                    logger.info(String.format("Rx: %s%n", line.get()));
+                    writeLine(line.get());
+                } else {
+                    throw new IOException("No se han recibido datos!");
+                }
             }
-
-            logger.info(String.format("server received %d bytes: %s%n", len, new String(data, 0, len)));
-            os.write(data, 0, len);
-            os.flush();
 
         } catch (Exception e) {
             logger.log(Level.SEVERE, "Error durante el procesado de la conexion", e);
