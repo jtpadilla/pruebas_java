0192784b9f104d96c2ef1137daec12c4076e9495
padilla <padilla@speedycontrol.com>
2022-03-05 18:04:57 +0100

    Nuevos ejemplos

--- tls/tls3/gypsyengineer/properties/client/src/main/java/com/gypsyengineer/tlsbunny/jsse/client/TLSv13TestClient.java
+++ tls/tls3/gypsyengineer/properties/client/src/main/java/com/gypsyengineer/tlsbunny/jsse/client/TLSv13TestClient.java
@@ -5,11 +5,9 @@ public class TLSv13TestClient {
     private static final String MESSAGE = "Like most of life's problems, this one can be solved with bending!";
 
     public static void main(String[] args) throws Exception {
-
         try (ClientSession clientSession = new ClientSession("localhost", 4443)) {
             clientSession.sendMessage(MESSAGE);
         }
-
     }
 
 }
