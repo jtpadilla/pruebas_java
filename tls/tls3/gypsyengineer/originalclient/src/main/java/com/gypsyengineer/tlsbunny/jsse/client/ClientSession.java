b7c297a63f7c530cc6e17ef2746f67581aa7775b
padilla <padilla@speedycontrol.com>
2022-03-05 17:48:25 +0100

    Nuevos ejemplos

--- tls/tls3/gypsyengineer/originalclient/src/main/java/com/gypsyengineer/tlsbunny/jsse/client/ClientSession.java
+++ tls/tls3/gypsyengineer/originalclient/src/main/java/com/gypsyengineer/tlsbunny/jsse/client/ClientSession.java
@@ -3,6 +3,8 @@ package com.gypsyengineer.tlsbunny.jsse.client;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 public class ClientSession implements AutoCloseable {
 
@@ -19,38 +21,51 @@ public class ClientSession implements AutoCloseable {
     final private OutputStream os;
 
     public ClientSession(String host, int port) throws IOException {
-
         socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket(host, port);
-
+        System.out.println("WantClientAuth:" + socket.getWantClientAuth());
+        System.out.println("NeedClientAuth:" + socket.getNeedClientAuth());
+        System.out.println("UseClientMode:" + socket.getUseClientMode());
+        System.out.println("EnableSessionCreation:" + socket.getEnableSessionCreation());
         socket.setEnabledProtocols(ENABLED_PROTOCOLS);
         socket.setEnabledCipherSuites(ENABLED_CLIPHER_SUITS);
-
-        System.out.println("WantClientAuth:" + socket.getWantClientAuth());
-        socket.setWantClientAuth(socket.getWantClientAuth());
-
-        System.out.println("NeedClientAuth:" + socket.getNeedClientAuth());
-        socket.setNeedClientAuth(socket.getNeedClientAuth());
-
-        System.out.println("UseClientMode:" + socket.getUseClientMode());
-        socket.setUseClientMode(socket.getUseClientMode());
-
-        System.out.println("EnableSessionCreation:" + socket.getEnableSessionCreation());
-        socket.setEnableSessionCreation(socket.getEnableSessionCreation());
-
         is = new BufferedInputStream(socket.getInputStream());
         os = new BufferedOutputStream(socket.getOutputStream());
     }
 
     public void sendMessage(String message) throws IOException {
-        os.write(message.getBytes());
-        os.flush();
+
+        // Envio
+        writeMessage(message);
+
+        // Recepcion
+        Optional<String> received = readMessage();
+
+        // Verificacion
+        if (received.isPresent()) {
+            System.out.println(received.get().equals(message) ? "OK" : "ERROR");
+        } else {
+            throw new IOException("No se han ecibido datos");
+        }
+
+    }
+
+    private Optional<String> readMessage() throws IOException {
         byte[] data = new byte[2048];
         int len = is.read(data);
         if (len <= 0) {
-            throw new IOException("no data received");
+            return Optional.empty();
+        } else {
+            return Optional.of(new String(data, 0, len));
         }
-        System.out.printf("CLIENT-SESSION: Ha recibido %d bytes: %s%n", len, new String(data, 0, len));
     }
+
+    private void writeMessage(String msg) throws IOException {
+        byte[] bytes = msg.getBytes(StandardCharsets.UTF_8);
+        os.write(bytes, 0, bytes.length);
+        os.flush();
+    }
+
+
 
     @Override
     public void close() throws Exception {
